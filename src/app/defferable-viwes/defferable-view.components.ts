7f1eb506ac8743ce8a4803f43c4de56eec3d9544
Tomer Kedem <tompha1@hotmail.com>
2024-01-06 19:08:05 +0200

    with viewport

--- src/app/defferable-viwes/defferable-view.components.ts
+++ src/app/defferable-viwes/defferable-view.components.ts
@@ -5,11 +5,27 @@ import { FooComponent } from "./foo";
     standalone: true,
     selector: 'app-defferable-view',
     template:`
-        <h1>Defferable View</h1>        
-        @defer (on timer(2s)){
+        <h1>Defferable View</h1>  
+        <div class="blue"></div>   
+        @defer (on viewport){
             <foo/>
+        } @loading (after 1s; minimum 2s)
+        {
+            <h1>loading...</h1>
+        } @error {
+            <h1>Error</h1>
+        } @placeholder {
+            <h1>Foo component</h1>
         }
+      
         <button (click)="iSaySo = true">Load</button>
+    `,
+    styles:`
+    .blue {
+        background: blue;
+        height: 100vh;
+        width: 200px;        
+    }
     `,
     imports: [FooComponent]
 })
